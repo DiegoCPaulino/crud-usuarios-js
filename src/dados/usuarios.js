f5d9b19ac23a1883e393313c17a748a98e897162
Diego Paulino <diegopaulino.dev@gmail.com>
2026-01-27 00:23:54 -0300

    refactor(dados): anonimizar dados de usuários para evitar informações reais

--- src/dados/usuarios.js
+++ src/dados/usuarios.js
@@ -1,11 +1,11 @@
 let usuarios = [
-    {id: 1, nome: 'Diego Costa Paulino', cpf: '525.612.628-80', dataNasc: '13/03/2004'},
-    {id: 2, nome: 'Mariana Tarie Henrique', cpf: '519.022.208-03', dataNasc: '12/03/2004'},
-    {id: 4, nome: 'Douglas Barbosa Paulino', cpf: '179.932.908-99', dataNasc: '27/06/1977'}
+    {id: 1, nome: 'Diego Costa Paulino', cpf: '109.885.858-12', dataNasc: '13/03/2004'},
+    {id: 2, nome: 'Rafael da Silva Gomes', cpf: '549.553.278-34', dataNasc: '27/12/1994'},
+    {id: 4, nome: 'Julia Ferreira de Castro', cpf: '083.089.298-26', dataNasc: '04/01/1948'}
 ]
 
 let usuariosDeletados = [
-    {id: 3, nome: 'Danielle Cristina Costa Paulino', cpf: '288.179.848-92', dataNasc: '25/11/1981'}
+    {id: 3, nome: 'Marcia Oliveira Rodrigues', cpf: '872.420.248-70', dataNasc: '23/01/1982'}
 ]
 
 module.exports = {
