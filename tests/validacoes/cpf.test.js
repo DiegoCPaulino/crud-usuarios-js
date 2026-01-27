ba774b63d4eb20924950b190509dd79ece702bde
Diego Paulino <diegopaulino.dev@gmail.com>
2026-01-27 17:33:03 -0300

    chore(testes): reorganiza o objeto de resposta para melhor leitura (sem impacto funcional)

--- tests/validacoes/cpf.test.js
+++ tests/validacoes/cpf.test.js
@@ -6,37 +6,61 @@ console.log("===== INÍCIO DOS TESTES DE CPF =====")
 
 testar(
   "CPF válido (formatado)",
-  criarResposta({ status: true, code: 200, message: null, data: null }),
+  criarResposta({
+    status: true,
+    code: 200,
+    message: null,
+    data: null }),
   validaCPF("047.484.388-27")
 );
 
 testar(
   "CPF válido (sem formatação)",
-  criarResposta({ status: true, code: 200, message: null, data: null }),
+  criarResposta({
+    status: true,
+    code: 200,
+    message: null,
+    data: null }),
   validaCPF("04748438827")
 );
 
 testar(
   "CPF com dígitos inválidos",
-  criarResposta({ status: false, code: 400, message: ERROS_CPF.DIGITOS_INVALIDOS, data: null }),
+  criarResposta({
+    status: false,
+    code: 400,
+    message: ERROS_CPF.DIGITOS_INVALIDOS,
+    data: null }),
   validaCPF("047.484.388-17")
 );
 
 testar(
   "CPF com dígitos repetidos",
-  criarResposta({ status: false, code: 400, message: ERROS_CPF.DIGITOS_REPETIDOS, data: null }),
+  criarResposta({
+    status: false, 
+    ode: 400,
+    message: ERROS_CPF.DIGITOS_REPETIDOS,
+    data: null }),
   validaCPF("555.555.555-55")
 );
 
 testar(
   "CPF com tamanho inválido",
-  criarResposta({ status: false, code: 400, message: ERROS_CPF.TAMANHO_INVALIDO, data: null }),
+  criarResposta({
+    status: false,
+    code: 400,
+    message: ERROS_CPF.TAMANHO_INVALIDO,
+    data: null }),
   validaCPF("047.484.388-179")
 );
 
 testar(
   "CPF vazio",
-  criarResposta({ status: false, code: 400, message: ERROS_CPF.CAMPO_OBRIGATORIO, data: null }),
+  criarResposta({
+    status: false,
+    code: 400,
+    message: ERROS_CPF.CAMPO_OBRIGATORIO,
+    data: null }),
   validaCPF("")
 );
 
@@ -46,7 +70,11 @@ Essa função retorna status: 'ERROR' pois não existe um tratamento de erro par
 ----------------------------------------------------------------------------------------------------
 testar(
     "CPF com caracteres especiais",
-    criarResposta({ status: false, code: 400, message: ERROS_CPF.DIGITOS_INVALIDOS, data: null }),
+    criarResposta({
+    status: false,
+    code: 400,
+    message: ERROS_CPF.DIGITOS_INVALIDOS,
+    data: null }),
     validaCPF("047.484.%88-27")
 );
 */
