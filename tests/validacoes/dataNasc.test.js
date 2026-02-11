75209e14de92f6b2d710984ce32e76bf84f2f035
Diego Paulino <diegopaulino.dev@gmail.com>
2026-02-11 01:26:11 -0300

    style: ajusta identação nos testes de validação de data de nascimento

--- tests/validacoes/dataNasc.test.js
+++ tests/validacoes/dataNasc.test.js
@@ -10,7 +10,7 @@ testar(
         code: 200,
         message: null,
         data: null }),
-        validaDataNasc('13/03/2004')
+    validaDataNasc('13/03/2004')
 );
 
 testar(
@@ -20,7 +20,7 @@ testar(
         code: 400,
         message: ERROS_DATA_NASC.FORMATO_INVALIDO,
         data: null }),
-        validaDataNasc('13032004')
+    validaDataNasc('13032004')
 );
 
 testar(
@@ -30,5 +30,5 @@ testar(
         code: 400,
         message: ERROS_DATA_NASC.FUTURA,
         data: null }),
-        validaDataNasc('13/03/2100')
+    validaDataNasc('13/03/2100')
 );
